4730be31bb99cad19ef7812330b98e56263ffd75
刘坤 <liukun@fangdd.com>
2020-09-16 18:26:55 +0800

    [NFF] 添加Constant && 添加group以及version

---
+++ composinglibrary/src/main/java/me/xiba/composinglibrary/Constant.java
@@ -0,0 +1,10 @@
+package me.xiba.composinglibrary;
+
+/**
+ * @Author:liukun
+ * @Date: 2020-09-16 18:06
+ * @Description:
+ */
+public class Constanst {
+    public static final String TAG = "ComposingLibrary";
+}
